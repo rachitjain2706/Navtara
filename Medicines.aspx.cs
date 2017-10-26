6e284602f23decb18ad8edf924ddcad4c8846702
rachitjain2706 <rachit.jain2706@gmail.com>
2017-10-26 13:11:34 +0530

    UOdates made

--- Medicines.aspx.cs
+++ Medicines.aspx.cs
@@ -88,14 +88,14 @@ public partial class Default2 : System.Web.UI.Page
         }
     }
 
-    protected string vendorCodeGenerated(string name)
+    public string vendorCodeGenerated(string name)
     {
         string vendorCode = "";
         for(int i=0; i<3; i++)
         {
-            vendorCode += name[i];
+            vendorCode += name[i+10];
         }
-        for(int i=name.Length; i>name.Length-3; i--)
+        for(int i=name.Length-1; i >= name.Length-3; i--)
         {
             vendorCode += name[i];
         }
@@ -104,41 +104,83 @@ public partial class Default2 : System.Web.UI.Page
 
     public string medicineCodeGenerated(string name, string nam)
     {
-        string medicineCode = "ALE - ";
+        string medicineCode = "";
         for (int i = 0; i < 3; i++)
         {
             medicineCode += name[i];
         }
-        for (int i = name.Length; i > name.Length - 3; i--)
+        for (int j = nam.Length-1; j >= nam.Length - 3; j--)
         {
-            medicineCode += nam[i];
+            medicineCode += nam[j];
         }
-        //Response.Write("<script>alert(medicineCode)</script>");
-        l2.Text = medicineCode;
         return medicineCode;
+    }
+
+    public string vendorCode(string name)
+    {
+        string code = "";
+        for (int i = 0; i < 6; i++)
+        {
+            code += name[i];
+        }
+        return code;
     }
 
     protected void Button1_Click(object sender, EventArgs e)
     {
         SqlConnection con = new SqlConnection();
         con.ConnectionString = @"Data Source = (localdb)\MSSQLlocaldb; Initial Catalog = Navtara; Integrated Security = True";
-
+        //Response.Write("<script>alert('Hola')</script>");
         try
         {
             con.Open();
+            string med_code = medicineCodeGenerated(gen_name.Text, trade_name.Text);
+            //string vendor_code = vendorCodeGenerated(vendor_list.SelectedValue);
+            string vendor_code = vendorCode(vendor_list.SelectedValue);
             string query = "insert into medicine values(@medicine_code, @generic_name, @trade_name, @purchasing_price, @selling_price, @description, @vendor_code)";
             SqlCommand cmd1 = new SqlCommand(query, con);
-            string med_code = medicineCodeGenerated(gen_name.Text, trade_name.Text);
-            l2.Text = med_code;
             cmd1.Parameters.AddWithValue("@medicine_code", med_code);
             cmd1.Parameters.AddWithValue("@generic_name", gen_name.Text);
             cmd1.Parameters.AddWithValue("@trade_name", trade_name.Text);
             cmd1.Parameters.AddWithValue("@description", med_desc.Text);
             cmd1.Parameters.AddWithValue("@purchasing_price", pp.Text);
             cmd1.Parameters.AddWithValue("@selling_price", sp.Text);
-            //string name = vendor_list.SelectedIndex.ToString();
-            cmd1.Parameters.AddWithValue("@vendor_code", vendor_list.SelectedIndex);
-            cmd1.ExecuteNonQuery();
+            cmd1.Parameters.AddWithValue("@vendor_code", vendor_code);
+            int rows = cmd1.ExecuteNonQuery();
+            if(rows > 0)
+            {
+                Response.Write("<script>alert('Successful insertion')</script>");
+                gen_name.Text = "";
+                trade_name.Text = "";
+                med_desc.Text = "";
+                pp.Text = "";
+                sp.Text = "";
+            }
+            query = "select * from vendor where vendor_code=@vendor_code";
+            cmd1 = new SqlCommand(query, con);
+            cmd1.Parameters.AddWithValue("@vendor_code", vendor_code);
+            SqlDataReader reader;
+            reader = cmd1.ExecuteReader();
+            string address = "";
+            string vendor_name = "";
+
+            while (reader.Read())
+            {
+                address = reader["address"].ToString();
+                vendor_name = reader["vendor_name"].ToString();
+            }
+            l2.Text = address;
+            query = "insert into vendor values(@vendor_code, @vendor_name, @address, @med_code)";
+            cmd1 = new SqlCommand(query, con);
+            cmd1.Parameters.AddWithValue("@vendor_code", vendor_code);
+            cmd1.Parameters.AddWithValue("@vendor_name", vendor_name);
+            cmd1.Parameters.AddWithValue("@address", address);
+            cmd1.Parameters.AddWithValue("@med_code", med_code);
+            rows = cmd1.ExecuteNonQuery();
+            if(rows > 0)
+            {
+                Response.Write("<script>alert('Lowla')</script>");
+            }
         }
         catch(Exception exc)
         {
