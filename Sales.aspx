bfbb2d0861ec33c8fd050df580d0d536af069194
rachitjain2706 <rachit.jain2706@gmail.com>
2017-10-26 19:28:54 +0530

    Sales work 1

---
+++ Sales.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage2.master" AutoEventWireup="true" CodeFile="Sales.aspx.cs" Inherits="Sales" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+
+    <div class="exTab3 container">	
+            <ul  class="nav nav-pills">
+			    <li class="active"><a  href="#1b" data-toggle="tab">New Sale</a></li>
+			    <li><a href="#2b" data-toggle="tab">Graphs</a></li>
+			    <li><a href="#3b" data-toggle="tab">Total Monthly Sale</a></li>
+		    </ul>
+
+			<div class="tab-content clearfix">
+			    <div class="tab-pane active" id="1b">
+                    <asp:Table runat="server" CssClass="table table-bordered" id="new_row">                        
+                        <asp:TableRow>
+                          <asp:TableCell Text="Medicine"></asp:TableCell>
+                          <asp:TableCell>
+                              <asp:DropDownList style="color:black;" runat="server" ID="DropDownList1">
+                              </asp:DropDownList>
+                          </asp:TableCell>
+                        </asp:TableRow>
+                        <asp:TableRow>
+                          <asp:TableCell Text="Quantity"></asp:TableCell>
+                          <asp:TableCell><asp:TextBox style="color:black;" runat="server" ID="TextBox1"></asp:TextBox></asp:TableCell>
+                        </asp:TableRow>
+                        <asp:TableRow>
+                          <asp:TableCell Text="Cost"></asp:TableCell>
+                          <asp:TableCell><asp:TextBox style="color:black;" runat="server" ID="TextBox2"></asp:TextBox></asp:TableCell>
+                        </asp:TableRow>
+                    </asp:Table>
+                    <asp:Button ID="Button2" runat="server" CssClass="btn btn-primary" Text="New Item" />
+                    <asp:Button ID="Button3" runat="server" CssClass="btn btn-primary" Text="Submit" OnClick="Button3_Click" /><br /><br /><br />
+                    <h3>Receipt</h3><br /><asp:Table runat="server" ID="show_receipt" CssClass="table table-bordered"></asp:Table>
+			    </div>
+                <div class="tab-pane" id="2b">
+                    
+				</div>
+                <div class="tab-pane" id="3b">
+                    <asp:Table runat="server" CssClass="table table-bordered" ID="t3">
+                        <asp:TableHeaderRow>
+                          <asp:TableHeaderCell Text="Medicine Name"></asp:TableHeaderCell>
+                          <asp:TableHeaderCell Text="Quantity"></asp:TableHeaderCell>
+                          <asp:TableHeaderCell Text="Purchase Date"></asp:TableHeaderCell>
+                          <asp:TableHeaderCell Text="Expiry Date"></asp:TableHeaderCell>
+                          <asp:TableHeaderCell Text="Batch Number"></asp:TableHeaderCell>
+                          <asp:TableHeaderCell Text="Vendor Name"></asp:TableHeaderCell>
+                        </asp:TableHeaderRow>
+                    </asp:Table>
+		        </div>
+			</div>
+        </div>
+
+
+
+</asp:Content>
+
---
+++ Sales.aspx.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class Sales : System.Web.UI.Page
+{
+    public int count = 0;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source = (localdb)\MSSQLlocaldb; Initial Catalog = Navtara; Integrated Security = True";
+            try
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("Select distinct medicine_code, generic_name from medicine", con);
+                SqlDataReader reader;
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    DropDownList1.Items.Add(reader["medicine_code"].ToString() + " - " + reader["generic_name"].ToString());
+                }
+            }
+            catch (Exception exc)
+            {
+
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+
+    /*protected void Button2_Click(object sender, EventArgs e)
+    {
+        count++;
+        TableRow row = new TableRow();
+        TableCell cell = new TableCell();
+        cell.Text = "Medicine";
+        DropDownList dropDownList = new DropDownList();
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = @"Data Source = (localdb)\MSSQLlocaldb; Initial Catalog = MedPlus; Integrated Security = True";
+        try
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select distinct medicine_code, generic_name from medicine", con);
+            SqlDataReader reader;
+            reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                //dropDownList.ID = "";
+                dropDownList.Items.Add(new ListItem(reader["medicine_code"].ToString() + " - " + reader["generic_name"].ToString(), reader["medicine_code"].ToString()));
+                //dropDownList.Style.Add()
+            }
+            TableCell tableCell = new TableCell();
+            tableCell.Controls.Add(dropDownList);
+
+            row.Cells.Add(cell);
+            row.Cells.Add(tableCell);
+            new_row.Rows.Add(row);
+
+            row = new TableRow();
+            cell = new TableCell();
+            cell.Text = "Quantity";
+            tableCell = new TableCell();
+            TextBox tb = new TextBox();
+            tableCell.Controls.Add(tb);
+            row.Cells.Add(cell);
+            row.Cells.Add(tableCell);
+            new_row.Rows.Add(row);
+
+            row = new TableRow();
+            cell = new TableCell();
+            cell.Text = "Cost";
+            tableCell = new TableCell();
+            tb = new TextBox();
+            tableCell.Controls.Add(tb);
+            row.Cells.Add(cell);
+            row.Cells.Add(tableCell);
+            new_row.Rows.Add(row);
+        }
+        catch(Exception exc)
+        {
+
+        }
+        finally
+        {
+            con.Close();
+        }
+    }*/
+
+    /*public void insertIntoSales(SqlConnection con)
+    {
+        SqlCommand cmd = new SqlCommand("insert into sales (medicine_code, quantity, price, sale_date) values(@med_code, @quan, @price, @date)", con);
+        cmd.Parameters.AddWithValue("@med_code", );
+        cmd.Parameters.AddWithValue("@quan", );
+        cmd.Parameters.AddWithValue("@price", );
+        cmd.Parameters.AddWithValue("@date", );
+        int rows = cmd.ExecuteNonQuery();
+        if(rows > 0)
+        {
+            Response.Write("<script>alert('Successful insertion')</script>");
+        }
+    }*/
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        //SqlConnection con = new SqlConnection();
+        //con.ConnectionString = @"Data Source = (localdb)\MSSQLlocaldb; Initial Catalog = MedPlus; Integrated Security = True";
+        /*try
+        {
+            con.Open();
+            insertIntoSales(con);
+            SqlCommand cmd = new SqlCommand("Select distinct medicine_code, generic_name from medicine", con);
+            SqlDataReader reader;
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                DropDownList1.Items.Add(reader["medicine_code"].ToString() + " - " + reader["generic_name"].ToString());
+            }
+        }
+        catch (Exception exc)
+        {
+
+        }
+        finally
+        {
+            con.Close();
+        }*/
+
+        //Table table = new Table();
+        TableHeaderRow tableHeader = new TableHeaderRow();
+        TableHeaderCell tableHeaderCell = new TableHeaderCell();
+        tableHeaderCell.Text = "Medicine";
+        tableHeader.Cells.Add(tableHeaderCell);
+        tableHeaderCell = new TableHeaderCell();
+        tableHeaderCell.Text = "Quantity";
+        tableHeader.Cells.Add(tableHeaderCell);
+        tableHeaderCell = new TableHeaderCell();
+        tableHeaderCell.Text = "Cost";
+        tableHeader.Cells.Add(tableHeaderCell);
+        tableHeaderCell = new TableHeaderCell();
+        tableHeaderCell.Text = "Total cost";
+        tableHeader.Cells.Add(tableHeaderCell);
+        show_receipt.Rows.Add(tableHeader);
+
+        TableRow row = new TableRow();
+        TableCell cell = new TableCell();
+        cell.Text = DropDownList1.SelectedValue;
+        row.Cells.Add(cell);
+        cell = new TableCell();
+        cell.Text = TextBox1.Text;
+        row.Cells.Add(cell);
+        cell = new TableCell();
+        cell.Text = TextBox2.Text;
+        row.Cells.Add(cell);
+        cell = new TableCell();
+        cell = new TableCell();
+        int val1, val2;
+        int.TryParse(TextBox1.Text, out val1);
+        int.TryParse(TextBox2.Text, out val2);
+        cell.Text = (val1 * val2).ToString();
+        row.Cells.Add(cell);
+        show_receipt.Rows.Add(row);
+    }
+}
